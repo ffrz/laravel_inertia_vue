f0833894615dad47a2d92e95699c807764a0ed93
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-06-25 20:19:00 +0700

    authorization

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,35 @@
 <script setup>
+import { ref, watch } from 'vue';
 import Pagination from '../Components/Pagination.vue';
+import { router } from '@inertiajs/vue3';
+import { debounce } from 'lodash';
 
-defineProps({
-  users: Object
+const props = defineProps({
+  users: Object,
+  search: String,
+  acl: Object,
 })
 
+const search = ref(props.search)
+watch(search, debounce((q) =>
+  router.get('/dashboard', { search: q }, { preserveState: true }), 500))
 const formatDate = (date) =>
   new Date(date).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
   })
+
 </script>
 
 <template>
+
   <Head title="Dashboard" />
+  <div class="flex justify-end mb-4">
+    <div class="w-1/4">
+      <input type="search" placeholder="Search" v-model="search">
+    </div>
+  </div>
   <table>
     <thead class="bg-slate-300">
       <tr>
@@ -22,6 +37,7 @@ const formatDate = (date) =>
         <th>Name</th>
         <th>Email</th>
         <th>Registration Date</th>
+        <th v-if="acl.can_delete_user">Delete</th>
       </tr>
     </thead>
     <tbody>
@@ -31,6 +47,7 @@ const formatDate = (date) =>
         <td>{{ user.name }}</td>
         <td>{{ user.email }}</td>
         <td>{{ formatDate(user.created_at) }}</td>
+        <td v-if="acl.can_delete_user"><button class="bg-red-500 w-6 h-6 rounded-full"></button></td>
       </tr>
     </tbody>
   </table>
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,26 @@
 
 use App\Http\Controllers\AuthController;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::inertia('/', 'Home')->name('home');
 Route::middleware('auth')->group(function () {
-    Route::inertia('/dashboard', 'Dashboard', ['users' => User::paginate(5, ['id', 'name', 'email', 'created_at'])])->name('dashboard');
+    Route::get('/dashboard', function (Request $request) {
+        return inertia('Dashboard', [
+            'users' => User::when(
+                $request->search,
+                function ($query) use ($request) {
+                    $query->where('name', 'like', '%' . $request->search . '%');
+                    $query->orWhere('email', 'like', '%' . $request->search . '%');
+                }
+            )->paginate(5, ['id', 'name', 'email', 'created_at'])->withQueryString(),
+            'search' => $request->search,
+            'acl' => ['can_delete_user' => Auth::user()->can('delete', User::class) ]
+        ]);
+    })->name('dashboard');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
